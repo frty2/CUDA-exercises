182b6d70c41f315573051d9de9e9bf83e387ed1c
Martin Pfeiffer <martinp.dev@googlemail.com>
2011-04-19 12:03:23 +0200

    exercise1

---
+++ exercise1/src/emptykernel.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+__global__ void emptyKernel() {
+	printf("empty kernel call\n");
+}
+
+int main() {
+    	dim3 threadsPerBlock(1);
+    	dim3 blocksPerGrid(1);
+    	emptyKernel<<<blocksPerGrid, threadsPerBlock>>>();
+    	cudaThreadSynchronize();
+	return 0;
+}
